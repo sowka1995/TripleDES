bc3fdf7465237ec0bd618fed252e0d0fe7df9280
Bartosz Sowa <b.sowa9@gmail.com>
2018-05-27 13:52:55 +0200

    Add Solution

---
+++ TripleDES/3DES/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DES
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string inputHex = "123456ABCD132536"; 
+            string keyHex = "A2BB09182736CCDDAABE09182736CA992AABB09182936CCDA";
+
+            string cipher = TripleDES.Encrypt(keyHex, inputHex);
+            Console.WriteLine(cipher);
+
+            string decrypted = TripleDES.Decrypt(keyHex, cipher);
+            Console.WriteLine(decrypted);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TripleDES/3DES/TripleDES.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DES
+{
+    public static class TripleDES
+    {
+        public static string Encrypt(string keyHex, string strHexToEncrypt)
+        {
+            var subKeys = SplitKey(keyHex);
+
+            string cipher1 = DES.Encrypt(subKeys.Item1, strHexToEncrypt);
+            string cipher2 = DES.Decrypt(subKeys.Item2, cipher1);
+            string cipher3 = DES.Encrypt(subKeys.Item3, cipher2);
+
+            return cipher3;
+        }
+
+        public static string Decrypt(string keyHex, string cipher)
+        {
+            var subKeys = SplitKey(keyHex);
+
+            string decrypted3 = DES.Decrypt(subKeys.Item3, cipher);
+            string decrypted2 = DES.Encrypt(subKeys.Item2, decrypted3);
+            string decrypted1 = DES.Decrypt(subKeys.Item1, decrypted2);
+
+            return decrypted1;
+        }
+
+        private static Tuple<string, string, string> SplitKey(string keyHex)
+        {
+            string key1 = keyHex.Substring(0, 16);
+            string key2 = keyHex.Substring(16, 16);
+            string key3 = keyHex.Substring(32, 16);
+
+            return new Tuple<string, string, string>(key1, key2, key3);
+        }
+    }
+}
