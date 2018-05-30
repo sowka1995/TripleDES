1938e916a840f1bd807c44d2cc647238d30a4133
Bartosz Sowa <b.sowa9@gmail.com>
2018-05-30 22:59:40 +0200

    Add new project to solution 3DES_GUI

---
+++ TripleDES/3DES_GUI/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace _3DES_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnKeyGenerator = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDecrypt.Location = new System.Drawing.Point(11, 273);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(183, 77);
+            this.btnDecrypt.TabIndex = 0;
+            this.btnDecrypt.Text = "Deszyfruj";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnEncrypt.Location = new System.Drawing.Point(200, 273);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(183, 77);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Szyfruj";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnKeyGenerator
+            // 
+            this.btnKeyGenerator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnKeyGenerator.Location = new System.Drawing.Point(12, 7);
+            this.btnKeyGenerator.Name = "btnKeyGenerator";
+            this.btnKeyGenerator.Size = new System.Drawing.Size(159, 29);
+            this.btnKeyGenerator.TabIndex = 19;
+            this.btnKeyGenerator.Text = "Generuj nowy klucz";
+            this.btnKeyGenerator.UseVisualStyleBackColor = true;
+            this.btnKeyGenerator.Click += new System.EventHandler(this.btnKeyGenerator_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(8, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 24);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Treść wejściowa";
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelKey.Location = new System.Drawing.Point(8, 63);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(72, 20);
+            this.labelKey.TabIndex = 17;
+            this.labelKey.Text = "Key label";
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelInfo.Location = new System.Drawing.Point(62, 211);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(242, 24);
+            this.labelInfo.TabIndex = 16;
+            this.labelInfo.Text = "zaszyfrowana/odszyfrowana";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(8, 211);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 24);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Treść";
+            // 
+            // textResult
+            // 
+            this.textResult.Location = new System.Drawing.Point(12, 238);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(484, 114);
+            this.textResult.TabIndex = 14;
+            this.textResult.DoubleClick += new System.EventHandler(this.textResult_DoubleClick);
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(12, 120);
+            this.textInput.Multiline = true;
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(484, 88);
+            this.textInput.TabIndex = 13;
+            this.textInput.DoubleClick += new System.EventHandler(this.textInput_DoubleClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(8, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Klucz prywatny";
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonEncrypt.Location = new System.Drawing.Point(313, 358);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(183, 46);
+            this.buttonEncrypt.TabIndex = 11;
+            this.buttonEncrypt.Text = "Szyfruj";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(12, 358);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(183, 46);
+            this.buttonDecrypt.TabIndex = 10;
+            this.buttonDecrypt.Text = "Deszyfruj";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 410);
+            this.Controls.Add(this.btnKeyGenerator);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.btnDecrypt);
+            this.Name = "Form1";
+            this.Text = "TripleDES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnKeyGenerator;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+    }
+}
+
---
+++ TripleDES/3DES_GUI/Form1.cs
@@ -0,0 +1,70 @@
+﻿using _3DES;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3DES_GUI
+{
+    public partial class Form1 : Form
+    {
+        private string keyHex;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            keyHex = TripleDES.GetRandomKeyHex();
+            labelKey.Text = keyHex;
+        }
+
+        private void btnKeyGenerator_Click(object sender, EventArgs e)
+        {
+            keyHex = TripleDES.GetRandomKeyHex();
+            labelKey.Text = keyHex;
+        }
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            string toEncrypt = textInput.Text;
+            string toEncryptHex = HexConverter.StrToHex(toEncrypt);
+            string encryptedHex = TripleDES.Encrypt(keyHex, toEncryptHex);
+            textResult.Text = encryptedHex;
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            string toDecrypt = textInput.Text;
+            string decryptedText = TripleDES.Decrypt(keyHex, toDecrypt);
+            textResult.Text = decryptedText;
+        }
+
+        private void textInput_DoubleClick(object sender, EventArgs e)
+        {
+            textInput.SelectAll();
+        }
+
+        private void textResult_DoubleClick(object sender, EventArgs e)
+        {
+            textResult.SelectAll();
+        }
+    }
+}
